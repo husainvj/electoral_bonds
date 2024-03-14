44106bdc11c795444bfdd3dea7f5e9e3680c8374
husainvj <husainvj@gmail.com>
2024-03-14 20:34:23 +0100

    phase 2 slider try

--- app.py
+++ app.py
@@ -2,9 +2,54 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import dash
+from dash import html, dcc, Input, Output
+import plotly.graph_objects as go
 
 # Load the dataset
-bonds=pd.read_csv('assets/bond_details.csv', sep=';')
-party=pd.read_csv('assets/party_details.csv', sep=';')
+df=pd.read_csv('assets/bond_details.csv', sep=',', parse_dates=['Date_of_Purchase'])
+#party=pd.read_csv('assets/party_details.csv', sep=',')
+df["Denomination"] = df["Denomination"].astype(float)
 
-party   
+
+
+
+# Convert 'date_of_purchase' to datetime and extract the year
+#df['Date_of_Purchase'] = pd.to_datetime(df['date_of_purchase'])
+df['Year'] = df['Date_of_Purchase'].dt.year
+df['Month'] = df['Date_of_Purchase'].dt.month
+# Initialize Dash app
+app = dash.Dash(__name__)
+
+# App layout
+app.layout = html.Div([
+    dcc.Slider(
+        id='year-slider',
+        min=df['Year'].min(),
+        max=df['Year'].max(),
+        value=df['Year'].min(),
+        marks={str(year): str(year) for year in df['Year'].unique()},
+        step=None
+    ),
+    dcc.Graph(id='treemap')
+])
+
+# Callback to update treemap based on slider input
+@app.callback(
+    Output('treemap', 'figure'),
+    [Input('year-slider', 'value')]
+)
+def update_figure(selected_time):
+    filtered_df = df[df['Date_of_Purchase'] == selected_time]
+    #bonds_tree= df.groupby("Purchaser_Name")["Denomination"].apply(sum).to_dict()
+    fig = go.Figure(go.Treemap(
+        labels=filtered_df['Purchaser_Name'],
+        values=filtered_df['Denomination'],
+        textinfo="label+value+percent root"
+    ))
+    fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
+    return fig
+
+# Run app
+if __name__ == '__main__':
+    app.run_server(debug=True)
