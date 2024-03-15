678fea518e2defd5daf8024a085284326536d06f
husainvj <husainvj@gmail.com>
2024-03-15 13:47:06 +0100

    working slider in dash app

--- app.py
+++ app.py
@@ -10,28 +10,32 @@ import plotly.graph_objects as go
 df=pd.read_csv('assets/bond_details.csv', sep=',', parse_dates=['Date_of_Purchase'])
 #party=pd.read_csv('assets/party_details.csv', sep=',')
 df["Denomination"] = df["Denomination"].astype(float)
+df['Month_Year'] = df['Date_of_Purchase'].dt.strftime('%b-%Y')
 
-
+#unique_dates=df.Month_Year.unique()
+#unique_dates
 
 
 # Convert 'date_of_purchase' to datetime and extract the year
 #df['Date_of_Purchase'] = pd.to_datetime(df['date_of_purchase'])
-df['Year'] = df['Date_of_Purchase'].dt.year
-df['Month'] = df['Date_of_Purchase'].dt.month
+#df['Year'] = df['Date_of_Purchase'].dt.year
+#df['Month'] = df['Date_of_Purchase'].dt.month
 # Initialize Dash app
 app = dash.Dash(__name__)
 
 # App layout
 app.layout = html.Div([
-    dcc.Slider(
+    dcc.Graph(id='treemap'),
+    dcc.RangeSlider(
         id='year-slider',
-        min=df['Year'].min(),
-        max=df['Year'].max(),
-        value=df['Year'].min(),
-        marks={str(year): str(year) for year in df['Year'].unique()},
-        step=None
-    ),
-    dcc.Graph(id='treemap')
+        min=0,
+        max=len(df['Month_Year'].unique())-1,
+        value=[0,10],
+        marks={i: df['Month_Year'].unique()[i] for i in range(len(df['Month_Year'].unique()))},
+        step=1,
+        allowCross=False
+    )
+    
 ])
 
 # Callback to update treemap based on slider input
@@ -40,15 +44,27 @@ app.layout = html.Div([
     [Input('year-slider', 'value')]
 )
 def update_figure(selected_time):
-    filtered_df = df[df['Date_of_Purchase'] == selected_time]
-    #bonds_tree= df.groupby("Purchaser_Name")["Denomination"].apply(sum).to_dict()
+    
+    m_y_start = df['Month_Year'].unique()[selected_time[0]]
+    m_y_end = df['Month_Year'].unique()[selected_time[1]]
+    idx_start = df.index[df['Month_Year'] == m_y_start][0]
+    idx_end = df.index[df['Month_Year'] == m_y_end][-1]
+
+    #filtered_df = df[df['Month_Year'].between(m_y_start, m_y_end)]
+
+    filtered_df = df.loc[idx_start : idx_end]
+    
+    dict_b= filtered_df.groupby("Purchaser_Name")["Denomination"].apply(sum).to_dict()
     fig = go.Figure(go.Treemap(
-        labels=filtered_df['Purchaser_Name'],
-        values=filtered_df['Denomination'],
-        textinfo="label+value+percent root"
+        labels = list(dict_b.keys()),
+        parents = [""]*len(dict_b.keys()),   #[""]*len(bonds_tree.keys()
+        values = list(dict_b.values()),
+        textinfo = "label+value"
     ))
-    fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
+    fig.update_layout( width=700 , height= 700 , margin = dict(t=25, l=25, r=25, b=25))
     return fig
+
+
 
 # Run app
 if __name__ == '__main__':
